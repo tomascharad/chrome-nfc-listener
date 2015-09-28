cffd438a24bf4afe3bbd00c12830b3e28a7b20c0
Tomás Charad <tomas.chrad@gmail.com>
2015-09-28 18:32:18 -0300

    Finishing customizing listener

--- bg.js
+++ bg.js
@@ -6,11 +6,33 @@ chrome.runtime.onMessageExternal.addListener(function(message, sender, sendRespo
     driverDataReceived(buffer);
     buffer = '';
   }
+  if (buffer.match('.*PRESENT:\\$BUS\\$.*\\$.*')) {
+    busDataReceived(buffer);
+    buffer = '';
+  }
 });
 
 function driverDataReceived(driverData) {
-  console.log(driverData.match('[0-9]*-[0-9]|K|k')[0]);
+  sendMessage({driverId: driverData.match('[0-9]*-[0-9]|K|k')[0]});
+}
+
+function busDataReceived(busData) {
+  // console.log(driverData.match('$BUS.*$')[0]);
+  var busInternalCode = busData.match('\\$BUS.*\\$')[0].split('BUS')[2];
+  sendMessage({busInternalCode: busInternalCode.slice(0, busInternalCode.length - 1)});
+}
+
+function sendMessage (message) {
+  console.log(message);
+  chrome.tabs.query({}, function(tabs) {
+    tabs.forEach(function(tab) {
+      chrome.tabs.sendMessage(tab.id, message, function(response) {
+        if (response) {
+          console.log(response);
+        }
+      });
+    });
+  });
 }
 
 console.log('started');
-alert('started');
---
+++ contentScript.js
@@ -0,0 +1,14 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log(request);
+  event = {};
+  if (request.driverId) {
+    var event = new CustomEvent('dispatcherIdReceived', {detail: request});
+  } else if (request.busInternalCode) {
+    request.busInternalCode = parseInt(request.busInternalCode).toString();
+    var event = new CustomEvent('busInternalCodeReceived', {detail: request});
+  }
+  window.dispatchEvent(event);
+  sendResponse({farewell: 'Received', object: request});
+});
+
+console.log('Initialized content script!');
